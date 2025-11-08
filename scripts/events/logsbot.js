161057edf8ac50d28b7fe9da83db44af5b5294c8
chistytsu-stack <chistytsu@gmail.com>
2025-11-08 11:49:01 +0600

    Update logsbot.js

--- scripts/events/logsbot.js
+++ scripts/events/logsbot.js
@@ -14,13 +14,13 @@ module.exports = {
 
 	langs: {
 		vi: {
-			title: "====== Nhật ký bot ======",
+			title: "✦━━━━━━━Nhật ký bot━━━━━━━✦",
 			added: "\n✅\nSự kiện: bot được thêm vào nhóm mới\n- Người thêm: %1",
 			kicked: "\n❌\nSự kiện: bot bị kick\n- Người kick: %1",
 			footer: "\n- User ID: %1\n- Nhóm: %2\n- ID nhóm: %3\n- Thời gian: %4"
 		},
 		en: {
-			title: "====== 𝗕𝗼𝘁 𝗹𝗼𝗴𝘀 ======",
+			title: "✦━━━━━━━𝗕𝗼𝘁 𝗹𝗼𝗴𝘀━━━━━━━✦",
 			added: "\n✅\n 𝙴𝚟𝚎𝚗𝚝: 𝚋𝚘𝚝 𝚑𝚊𝚜 𝚋𝚎𝚎𝚗 𝚊𝚍𝚍𝚎𝚍 𝚝𝚘 𝚊 𝚗𝚎𝚠 𝚐𝚛𝚘𝚞𝚙\n- 𝙰𝚍𝚍𝚎𝚍 𝚋𝚢: %1",
 			kicked: "\n❌\n𝗘𝘃𝗲𝗻𝘁: 𝗯𝗼𝘁 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝗸𝗶𝗰𝗸𝗲𝗱\n- 𝗞𝗶𝗰𝗸𝗲𝗱 𝗯𝘆: %1",
 			footer: "\n- 𝚄𝚜𝚎𝚛 𝙸𝙳: %1\n- 𝙶𝚛𝚘𝚞𝚙: %2\n- 𝙶𝚛𝚘𝚞𝚙 𝙸𝙳: %3\n- 𝚃𝚒𝚖𝚎: %4"
